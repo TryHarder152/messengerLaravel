c27ce8b6c92a625a21445b560d61fbedad38f5f0
Oleg Kudelkin <tryhard.work@mail.ru>
2022-09-03 05:37:52 +0300

    init

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+
+Route::group(['namespace' => 'Main'], function() {
+    Route::get('/', 'IndexController')->name('main.index');
+});
+
+Route::group(['namespace' => 'Profile', 'prefix' => 'profile'], function() {
+    Route::get('/', 'IndexController')->name('profile.index');
+});
